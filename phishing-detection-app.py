d27660bce95a8df2f214bbcc6c54058a11481932
Kavitha <31727851+Kavitha7@users.noreply.github.com>
2023-11-13 16:10:02 +0530

    Add files via upload

--- phishing-detection-app.py
+++ phishing-detection-app.py
@@ -1,3 +1,10 @@
+import streamlit as st
+import numpy as np
+import pickle
+import re
+import random
+import socket
+from urllib.parse import urlparse
 st.title("Phishing Detector")
 st.subheader("Phishing Domain Detector Engine")
 with st.form("form1", clear_on_submit=False):
@@ -35,3 +42,4 @@ with st.form("form1", clear_on_submit=False):
             st.markdown('The Domin is Legitimate')
         else:
             st.markdown('The Domin is Malicious')
+
