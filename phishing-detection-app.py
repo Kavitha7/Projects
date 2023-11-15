cbe091125b4861ec84450e6621e832041cbb53ef
Kavitha <31727851+Kavitha7@users.noreply.github.com>
2023-11-15 16:10:29 +0530

    Add files via upload

--- phishing-detection-app.py
+++ phishing-detection-app.py
@@ -5,7 +5,7 @@ import re
 import random
 import socket
 from urllib.parse import urlparse
-model = pickle.load(open('y_pred', 'rb'))
+model = pickle.load(open('/app/projects/y_pred', 'rb'))
 st.title("Phishing Detector")
 st.subheader("Phishing Domain Detector Engine")
 with st.form("form1", clear_on_submit=False):
