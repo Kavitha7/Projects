7f1868ac06878fd4d6bcd7a96b16cd99b1faad18
Kavitha <31727851+Kavitha7@users.noreply.github.com>
2023-12-04 18:33:59 +0530

    Add files via upload

--- phishing-detection-app.py
+++ phishing-detection-app.py
@@ -10,12 +10,37 @@ st.title("Phishing Detector")
 st.subheader("Phishing Domain Detector Engine")
 with st.form("form1", clear_on_submit=False):
     text_input = st.text_input("Enter a URL")
-if st.form_submit_button("Submit"):
-    features = []
-    final_features = features.append(str(text_input))
-    prediction = ''.join(model.predict(final_features))
-    output = prediction[0]
-if output == 0:
-    st.markdown('The Domin is Legitimate')
-else:
-    st.markdown('The Domin is Malicious')
+
+    if st.form_submit_button("Go"):
+        domain = urlparse(text_input).netloc
+
+        text = "AaEeIiOoUu"
+        count = [i for i in str(domain) if i in text]
+        vowels = len(count)
+
+        length = len(domain)
+
+        address = socket.gethostbyname(domain)
+        ip = None
+        if address != None:
+            ip = 1
+        else:
+            ip = 0
+
+        lis= [0,1]
+        server = random.choice(lis)
+
+        sign = re.findall("[._/?=@&! ,+*#$%]", domain)
+        sign_count = len(sign)
+
+        features = []
+        final_features = features.append(str(text_input))
+        prediction = ''.join(model.predict(final_features))
+        output = prediction[0]
+
+        output = prediction[0]
+
+        if output == 0:
+            st.markdown('The Domin is Legitimate')
+        else:
+            st.markdown('The Domin is Malicious')
