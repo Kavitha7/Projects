07122ab6aa8216da0006589b4cc02dfe250c449f
Kavitha <31727851+Kavitha7@users.noreply.github.com>
2023-12-04 16:27:07 +0530

    Add files via upload

--- phishing-detection-app.py
+++ phishing-detection-app.py
@@ -10,11 +10,11 @@ st.title("Phishing Detector")
 st.subheader("Phishing Domain Detector Engine")
 with st.form("form1", clear_on_submit=False):
     text_input = st.text_input("Enter a URL")
-features = []
-final_features = features.append(str(text_input))
-final_features1 = [np.array(final_features)]
-prediction = model.predict(final_features1)
-output = prediction[0]
+     if st.form_submit_button("Go"):
+            features = []
+            final_features = features.append(str(text_input))
+            prediction = ''.join(model.predict(final_features))
+            output = prediction[0]
 if output == 0:
     st.markdown('The Domin is Legitimate')
 else:
